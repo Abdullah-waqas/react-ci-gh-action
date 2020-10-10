6c3ad0885d7bb28730fee89231c601165f28b560
abdullah.waqas <abdullah.waqas@10pearls.local>
2020-10-10 20:30:46 +0500

    Added h5 tag

--- src/TodoList.js
+++ src/TodoList.js
@@ -30,7 +30,7 @@ const ToDoList = () => {
 
   return (
     <div className="todo-list">
-      <h1>Todo list Application</h1>
+      <h5>Todo list Application</h5>
       <div data-testid="list-container">
         {tasks.map((task, index) => (
           <div data-testid="todo-item" className="todo" key={index}>
